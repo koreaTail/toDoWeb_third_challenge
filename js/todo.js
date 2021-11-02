e22b77b680a660c02e327d8cf7840ecafc5abfcb
leesangjun <prokjun@gmail.com>
2021-11-03 07:06:31 +0900

    #7.1 Adding ToDos (07:00)

--- js/todo.js
+++ js/todo.js
@@ -4,11 +4,22 @@ const ul = document.querySelector("#todo-list")
 
 todoForm.addEventListener("submit", 투두제출시할일)
 
+function 방금작성한할일화면에표시하기(방금작성한할일) {
+  console.log("과연", 방금작성한할일)
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = 방금작성한할일;
+  ul.appendChild(li);
+
+}
+
 function 투두제출시할일() {
   event.preventDefault();
   const 방금작성한할일 = input.value;
-  console.log(방금작성한할일, input.value);
+  // console.log(방금작성한할일, input.value);
   input.value = "";
-  console.log(방금작성한할일, input.value);
+  // console.log(방금작성한할일, input.value);
+  방금작성한할일화면에표시하기(방금작성한할일)
 }
 
