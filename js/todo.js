adbaa2c474410012fa34a45f7c47690a612d0f31
leesangjun <prokjun@gmail.com>
2021-11-03 07:34:00 +0900

    #7.2 Deleting To Dos (09:54)

--- js/todo.js
+++ js/todo.js
@@ -5,21 +5,28 @@ const ul = document.querySelector("#todo-list")
 todoForm.addEventListener("submit", 투두제출시할일)
 
 function 방금작성한할일화면에표시하기(방금작성한할일) {
-  console.log("과연", 방금작성한할일)
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = 방금작성한할일;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", 선택된할일삭제하기)
+  li.appendChild(span);
+  li.appendChild(button);
   ul.appendChild(li);
+}
 
+
+
+
+function 선택된할일삭제하기(e) {
+  e.target.parentNode.remove()
 }
 
 function 투두제출시할일() {
   event.preventDefault();
   const 방금작성한할일 = input.value;
-  // console.log(방금작성한할일, input.value);
   input.value = "";
-  // console.log(방금작성한할일, input.value);
   방금작성한할일화면에표시하기(방금작성한할일)
 }
 
