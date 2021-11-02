38a7d5d7a421bdce09d49bf7be8fd838c1a3b141
leesangjun <prokjun@gmail.com>
2021-11-03 06:23:48 +0900

    #7.0 Setup (07:55)

--- js/greetings.js
+++ js/greetings.js
@@ -8,33 +8,34 @@ const USERNAME = "username"
 
 const 저장된이름 = localStorage.getItem(USERNAME);
 
-if(저장된이름 !== null){
-    숨기기(로그인폼)
-    이름화면에표시하기(저장된이름)
-} else{
-    로그인폼.addEventListener("submit", 이름제출되면할일)
+if (저장된이름 !== null) {
+  숨기기(로그인폼)
+  이름화면에표시하기(저장된이름)
+} else {
+  로그인폼.addEventListener("submit", 이름제출되면할일)
 }
 
-function 이름제출되면할일(event){
-    event.preventDefault();
-    const username = 인풋.value;
-    이름로컬에저장하기(username)
-    이름화면에표시하기(username)
+function 이름제출되면할일(event) {
+  event.preventDefault();
+  const username = 인풋.value;
+  이름로컬에저장하기(username)
+  이름화면에표시하기(username)
+  숨기기(로그인폼)
 }
 
-function 이름화면에표시하기(보여줄이름){
-    h1.innerText = `안녕, ${보여줄이름}`;
-    숨기기취소(h1);
+function 이름화면에표시하기(보여줄이름) {
+  h1.innerText = `안녕, ${보여줄이름}`;
+  숨기기취소(h1);
 }
 
-function 숨기기(숨길놈){
-    숨길놈.classList.add(HIDDEN_CLASSNAME)
+function 숨기기(숨길놈) {
+  숨길놈.classList.add(HIDDEN_CLASSNAME)
 }
 
-function 숨기기취소(숨기기취소할놈){
-    숨기기취소할놈.classList.remove(HIDDEN_CLASSNAME)
+function 숨기기취소(숨기기취소할놈) {
+  숨기기취소할놈.classList.remove(HIDDEN_CLASSNAME)
 }
 
-function 이름로컬에저장하기(유저이름){
-    localStorage.setItem(USERNAME, 유저이름)
+function 이름로컬에저장하기(유저이름) {
+  localStorage.setItem(USERNAME, 유저이름)
 }
--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,14 @@
 const todoForm = document.querySelector("#todo-form")
+const input = todoForm.querySelector("input")
 const ul = document.querySelector("#todo-list")
 
 todoForm.addEventListener("submit", 투두제출시할일)
 
-function 투두제출시할일(){
-    event.preventDefault();
-    console.log("투두제출됨")
+function 투두제출시할일() {
+  event.preventDefault();
+  const 방금작성한할일 = input.value;
+  console.log(방금작성한할일, input.value);
+  input.value = "";
+  console.log(방금작성한할일, input.value);
 }
+
