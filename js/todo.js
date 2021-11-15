479dd00464890d0f80840e62d571651bf4e8b822
leesangjun <prokjun@gmail.com>
2021-11-15 11:32:58 +0900

    불필요한코드정리

--- js/todo.js
+++ js/todo.js
@@ -28,19 +28,9 @@ function 할일로컬에저장하기() {
 
  
 function 선택된할일삭제하기(e) {
-  console.log(parseInt(e.target.parentNode.id))
   e.target.parentNode.remove()
-  console.log(toDos.find(x => x.id === parseInt(e.target.parentNode.id)) ) 
   const 제거된할일목록 = toDos.filter(x => x.id !== parseInt(e.target.parentNode.id))  
   toDos = 제거된할일목록; 
-  할일로컬에저장하기()
-  // 이걸 지우면, 통째로 지워져.. 이렇게 할 수 없고, 새로 그리는 방식으로 해야할듯..
-  // localStorage.removeItem(TODOS_KEY)
-  // console.log(toDos)
-  // console.dir(e.target.parentElement.children[0])
-  // 몇번째꺼인지만 알아내면 된다.. 그순간. 알아야해.
-  // 몇번째껄 눌렀는지.. 타겟으로 알 수 없나..
-  // console.dir(ul.children[??????].innerText)
 }
 
 function 투두제출시할일(event) {
@@ -48,7 +38,6 @@ function 투두제출시할일(event) {
   const 방금작성한할일 = input.value;
   input.value = "";
 
-  // 로컬에 저장하기
   const 방금작성한할일객체화하기 = {
     text: 방금작성한할일,
     id: Date.now(),
@@ -63,7 +52,6 @@ function 투두제출시할일(event) {
 const 저장된할일 = localStorage.getItem(TODOS_KEY);
 
 if (저장된할일 !== null) {
-  // 저장된할일화면에표시하기(개별할일)
   JSON.parse(저장된할일).forEach((개별할일) => 방금작성한할일화면에표시하기(개별할일))
   toDos = JSON.parse(저장된할일)
 } else {
--- js/weather.js
+++ js/weather.js
@@ -16,7 +16,7 @@ function onGeoOk(position){
 }
 
 function onGeoError(){
-    alert("Can't find you. No weather for you");
+    // alert("Can't find you. No weather for you");
 }
 
 
